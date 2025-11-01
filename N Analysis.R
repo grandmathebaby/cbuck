13a8e3e018d75136c557445365af0d7029e66f38
grandmathebaby <sachamedjo@gmail.com>
2025-11-01 16:17:34 -0700

    we're workingggg

--- N Analysis.R
+++ N Analysis.R
@@ -16,3 +16,19 @@ display.all.moma()
 rm(list=ls())
 Ammonia <- read_csv("NH4PlotsInc.csv")
 View(Ammonia)
+ggplot(Ammonia, aes(x=concentration, y=absorbance)) + geom_point() + theme_bw()
+#--Nitrate
+rm(list=ls())
+Nitrate <- read_csv("NO3PlotsInc.csv")
+View(Nitrate)
+ggplot(Nitrate, aes(x=concentration, y=absorbance)) + geom_point() + theme_bw()
+#
+# Then do each type of correlation test
+cor.test(Nitrate$absorbance, Nitrate$concentration, method="pearson")
+# r = 0.57, p = 0.006
+
+cor.test(Nitrate$absorbance, Nitrate$concentration, method="spearman")
+# rho = 0.39, p = 0.07
+
+cor.test(Nitrate$absorbance, Nitrate$concentration, method="kendall")
+# tau = 0.25, p = 0.11
